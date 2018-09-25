332ecaab521003ee8322f478f535f472367fab04
johnmadakin <edafeadjeke@gmail.com>
2018-09-25 15:30:22 +0100

    feature: post menu route - created a postMenu function that adds food to the db - created a POST menu route - wrote test to validate, authenticate and post food
    [delivers #160647885]

--- server/middleware/validateRoutes.js
+++ server/middleware/validateRoutes.js
@@ -1,227 +1,195 @@
-import ValidateUser from './userValidation';
+import {
+  validateKeys,
+  validateKeysLength,
+  validateString,
+  validateImageUrl,
+  validateText,
+} from './userValidation';
 
-const validate = new ValidateUser();
-
-export default class Validator {
-  constructor() {
-    this.validateOrders = this.validateOrders.bind(this);
-    this.validateID = this.validateID.bind(this);
-    this.validatePrice = this.validatePrice.bind(this);
-    this.validateCalorie = this.validateCalorie.bind(this);
-    this.validateMenuRoute = this.validateMenuRoute.bind(this);
-  }
-
-  validateKeys(validKeys, keys, res) {
-    if (validKeys.length !== keys.length) {
-      return res.status(400).json({
-        status: "Failure",
-        message: "Incomplete or key length exceeded"
-      });
-    }
-    for (let key of keys) {
-      if (!validKeys.includes(key)) {
-        return res.status(400).json({
-          error: `${key} is an Invalid key`,
-        });
-      }
-    }
-    return true;
-  }
-
- validateUserID(id, res) {
-    if (!validate.validateID(id)){
-      return res.status(400).json({
-        status: "Failure",
-        message: `Invalid value for userID`,
-      });
-    }
-    return true;
-  }
-
-  validateStatus(status, validStatus, res) {
-    if (!validStatus.includes(status)) {
-      return res.status(400).json({
-        status: 'Failure',
-        message: 'Invalid data entry for status',
-      });
-    } 
-    return true;
-  }
-
-  validatePayment(payment, req, res, key) {
-    if (payment !== 'payondelivery') {
-      return res.status(400).json({
-        status: 'Failure',
-        message: `Invalid data entry! payment values can't be ${req.body[key]}`,
-      });
-    }
-    return true;
-  }
-
-  validateAddress(deliveryAddress, res) {
-    if (!validate.validateText(deliveryAddress)) {
-      return res.status(400).json({
-        status: 'Failure',
-        message: 'Invalid data entry! Check your Delivery Address field',
-      });
-    }
-    return true;
-  }
-
-  validateItemName(name,res) {
-    if(validate.validateString(name)){
-      return res.status(400).json({
-        status: 'failure',
-        message: 'Invalid Item Name, check your orders items',
-      });
-    }
-    return true;
-  }
-
-  validateOrdersItems(orders, res) {
-    for (let order of orders){
-      this.validateItemName(order.itemName);
-      if(typeof orders.price !== 'number' || !this.validateNumber(order.price) ){
-        return res.status(400).json({
-          status: 'failure',
-          message: `Invalid price value, check your orders items`,
-        });
-      }
-      if(typeof orders.quantity !== 'number' || !this.validateNumber(order.quantity) ){
-        return res.status(400).json({
-          status: 'failure',
-          message: `Invalid Quantity value, check your orders items`,
-        });
-      }
-    }
-    return true;
-  }
-
-  validatePrice(key, price, res) {
-    if (key === 'price' && !this.validateNumber(price)) {
-      return res.status(400).json({
-        status: 'failure',
-        message: 'Invalid price entry!',
-      });
-    }
-    return true;
-  }
-
-  validateCalorie(key, calorie, res) {
-    if (key === 'calorie' && !this.validateNumber(calorie)) {
-      return res.status(400).json({
-        status: 'failure',
-        message: 'Invalid Calorie entry!',
-      });
-    }
-    return true;
-  }
-  validateMenu(key, value, res) {
-    if (key === 'menu' && !menuValues.includes(value)) {
-      const menuValues = ['burgers','chicken', 'breakfast-packs', 'lunch-packs'];
-      return res.status(400).json({
-        status: 'failure',
-        message: `Invalid menu entry!`,
-      });
-    }
-    return true;
-  }
-
-  validateImage(key,image, res) {
-    if (key === 'imageUrl' && !validate.validateImageUrl(image)) {
-      return res.status(400).json({
-        status: 'failure',
-        message: 'Invalid image URL',
-      });
-    }
-    return true;
-  }
-
-  validateDescription(key, des, res) {
-    if (key === 'description' && !validate.validateText(des)) {
-      return res.status(400).json({
-        status: 'failure',
-        message: 'Invalid image URL',
-      });
-    }
-    return true;
-  }
-
-  validateIngredients(key, ingredients, res) {
-    const validText = /^[0-9a-zA-Z ,]+$/;
-    if (key === 'ingredients' && !validText.test(ingredients)) {
-      return res.status(400).json({
-        status: 'failure',
-        message: 'Invalid ingredient format! please separate items with a comma',
-      });
-    }
-    return true;
-  }
-
-  validateMenuRoute(req, res, next) {
-    const { name, price, calorie, menu, ingredients, description, imageUrl } = req.body;
-    const validKeys = ['name', 'price', 'calorie', 'menu', 'ingredients', 'description', 'imageUrl'];
-    const inputs = Object.keys(req.body);
-    this.validateKeys(validKeys, inputs, res);
-    for (let key of inputs) {
-      this.validatePrice(key,price,res);
-      this.validateMenu(key, menu,res);
-      this.validateCalorie(key,calorie,res);
-      if (key === 'name'){
-        this.validateItemName(name,res);
-      }
-      this.validateDescription(key, description,res);
-      this.validateImage(key,imageUrl,res);
-      this.validateIngredients(key,ingredients,res);
-    }
-  }
-    
-  validateOrders(req, res, next) {
-    const { userId, orders, status, payment, deliveryAddress } = req.body;
-    const validKeys = ['userId', 'orders', 'status', 'payment', 'deliveryAddress'];
-    const validStatus = ['pending', 'confirmed', 'accepted', 'declined'];
-    const inputs = Object.keys(res.body);
-    this.validateKeys(validKeys, inputs, res);
-    for (let key in inputs ) {
-      if (key === 'userId'){
-        this.validateUserID(userId,req,res)
-      }
-      if (key === 'status') {
-        this.validateStatus(status,validStatus,res);
-      }
-      if (key === 'payment') {
-        this.validatePayment(payment,req,res,key);
-      }
-      if (key === 'deliveryAddress') {
-        this.validateAddress(deliveryAddress, req, res);
-      }
-      if (key === 'orders' && !Array.isArray(req.body[key])) {
-        return res.status(400).json({
-          status: "Failure",
-          message: `Invalid data entry! orders should be of type array`,
-        });
-      }
-    }
-    this.validateOrdersItems(orders, res);
-    return next();
-  }
-
-  validateNumber(num) {
-    const reg = /^[0-9]+$/;
-    if (reg.test(num)) {
-      return true;
-    }
+const validateNumber = (number) => {
+  const validator = `${number}`;
+  if (validator.length > 6 || typeof number !== 'number') {
     return false;
   }
+  return true;
+};
 
-  validateID(req, res, next) {
-    const { id } = req.body;
-    if (typeof id !== 'number' || !this.validateNumber(id)) {
+const validateIngredients = (ingredients) => {
+  const validText = /^[0-9a-zA-Z ,]+$/;
+  if (!validText.test(ingredients)) {
+    return false;
+  }
+  return true;
+};
+
+const validateFoodName = (string) => {
+  if (typeof string !== 'string') return false;
+  if (string.length < 2 || string.length > 25) return false;
+  const validString = /^[a-zA-Z  -]+$/;
+  return string.trim().match(validString);
+}
+
+export const validateMenuRoute = (req, res, next) => {
+  const {
+    name,
+    price,
+    calorie,
+    menu,
+    ingredients,
+    description,
+    imageUrl,
+  } = req.body;
+  const validKeys = ['name', 'price', 'calorie', 'menu', 'ingredients', 'description', 'imageUrl'];
+  const validMenu = ['chicken', 'burgers', 'noddles', 'breverages'];
+  const inputs = Object.keys(req.body);
+  if (inputs.length > validKeys.length) {
+    return res.status(400).json({
+      error: 'invalid key',
+      message: 'number of keys exceeded',
+    });
+  }
+  if (inputs.length < validKeys.length) {
+    return res.status(400).json({
+      error: 'invalid key',
+      message: 'incomplete valid keys',
+    });
+  }
+  for (let i = 0; i < inputs.length; i++) {
+    if (validateKeys(inputs[i], validKeys) === false) {
       return res.status(400).json({
-        status: 'Failure',
-        message: 'Invalid Parameter Id',
+        status: 'invalid key',
+        message: 'please enter valid keys',
       });
     }
-    return next();
   }
+  if (!validateFoodName(name)) {
+    return res.status(400).json({
+      status: 'invalid data entry',
+      message: 'food name has an invalid value',
+    });
+  }
+  if (!validateNumber(price) || !validateNumber(calorie)) {
+    return res.status(400).json({
+      status: 'valid data entry',
+      message: 'price or calorie has an invalid value',
+    });
+  }
+  if (validateKeys(menu.toLowerCase(), validMenu) === false) {
+    return res.status(400).json({
+      status: 'invalid data entry',
+      message: 'please enter a valid value for menu',
+    });
+  }
+  if (!validateIngredients(ingredients)) {
+    return res.status(400).json({
+      status: 'invalid data entry',
+      message: 'Invalid ingredient format! please separate items with a comma',
+    });
+  }
+  if (!validateText(description)) {
+    return res.status(400).json({
+      status: 'invalid data entry',
+      message: 'decription has an invalid value',
+    });
+  }
+  if (!validateImageUrl(imageUrl)) {
+    return res.status(400).json({
+      status: 'invalid data entry',
+      message: 'imageurl has an invalid value',
+    });
+  }
+  return next();
+};
+
+
+const validateStatus = (status, validStatus, res)  => {
+  if (!validStatus.includes(status)) {
+    return res.status(400).json({
+      status: 'Failure',
+      message: 'Invalid data entry for status',
+    });
+  } 
+  return true;
+}
+
+const validatePayment = (payment, req, res, key) => {
+  if (payment !== 'payondelivery') {
+    return res.status(400).json({
+      status: 'Failure',
+      message: `Invalid data entry! payment values can't be ${req.body[key]}`,
+    });
+  }
+  return true;
+}
+
+const validateAddress = (deliveryAddress, res) => {
+  if (!validateText(deliveryAddress)) {
+    return res.status(400).json({
+      status: 'Failure',
+      message: 'Invalid data entry! Check your Delivery Address field',
+    });
+  }
+  return true;
+}
+
+const validateItemName = (name,res) => {
+  if (validateString(name)) {
+    return res.status(400).json({
+      status: 'failure',
+      message: 'Invalid Item Name, check your orders items',
+    });
+  }
+  return true;
+}
+
+export const validateOrdersItems = (orders, res) => {
+  for (let order of orders){
+    validateItemName(order.itemName);
+    if(typeof orders.price !== 'number' || !validateNumber(order.price) ){
+      return res.status(400).json({
+        status: 'failure',
+        message: `Invalid price value, check your orders items`,
+      });
+    }
+    if(typeof orders.quantity !== 'number' || !validateNumber(order.quantity) ){
+      return res.status(400).json({
+        status: 'failure',
+        message: `Invalid Quantity value, check your orders items`,
+      });
+    }
+  }
+  return true;
+}
+
+
+
+   
+const validateOrders = (req, res, next) => {
+  const { userId, orders, status, payment, deliveryAddress } = req.body;
+  const validKeys = ['userId', 'orders', 'status', 'payment', 'deliveryAddress'];
+  const validStatus = ['pending', 'confirmed', 'accepted', 'declined'];
+  const inputs = Object.keys(res.body);
+  validateKeys(validKeys, inputs, res);
+  for (let key in inputs ) {
+    if (key === 'userId'){
+      validateUserID(userId,req,res)
+    }
+    if (key === 'status') {
+      validateStatus(status,validStatus,res);
+    }
+    if (key === 'payment') {
+      validatePayment(payment,req,res,key);
+    }
+    if (key === 'deliveryAddress') {
+      validateAddress(deliveryAddress, req, res);
+    }
+    if (key === 'orders' && !Array.isArray(req.body[key])) {
+      return res.status(400).json({
+        status: "Failure",
+        message: `Invalid data entry! orders should be of type array`,
+      });
+    }
+  }
+  validateOrdersItems(orders, res);
+  return next();
 }
