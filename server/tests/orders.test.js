5c731d2326b95667aa0b24b9e26ae9cd755efb50
johnmadakin <edafeadjeke@gmail.com>
2018-09-27 19:41:02 +0100

    travis error fixed

--- server/tests/orders.test.js
+++ server/tests/orders.test.js
@@ -231,19 +231,6 @@ describe('POST menu to menu Route', () => {
   }); 
 });
 
-describe('Get all Orders', () => {
-  it('should get all the orders', (done) => {
-    request(app).get('/api/v1/orders')
-      .expect(200)
-      .expect((res) => {
-        const { data } = res.body;
-        expect(data.length).toBeGreaterThan(0);
-        expect(data.length).toBe(2);
-      })
-      .end(done);
-  });
-});
-
 describe('POST order Route', () => {
   const order = {
     orders: [{
