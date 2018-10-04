da5b22eaaec05d5aed53bcfa8e3d93e911de84a7
Dafe Omare <36252436+JohnMadakin@users.noreply.github.com>
2018-10-04 23:54:53 +0100

    chore: fixed validation for both sign up and sign up (#44)
     #160992125 fixed validation for both sign up and sign in

--- client/js/index.js
+++ client/js/index.js
@@ -297,6 +297,18 @@ const getItem = () => {
   }
 }
 
+const validateUserText = (string) => {
+  if (typeof string !== 'string') return false;
+  const validString = /^[0-9a-zA-Z_]+$/;
+  return string.trim().match(validString);
+};
+const validateStringLength = (string) => {
+  if (string.length >= 6 && string.length <= 300) {
+    return true;
+  }
+  return false;
+};
+
 
 const signIn = () => {
   const url = `${baseUrl}/api/v1/auth/login`;
@@ -304,8 +316,19 @@ const signIn = () => {
     e.preventDefault();
     const username = document.querySelector('#uname').value;
     const password = document.querySelector('#psw').value;
+    if (!validateUserText(username)) {
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `username is invalid`;
+      return loginMessage.style.display = 'block';
+    }
+    if (!validateStringLength(password)) {
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `password length is too short`;
+      return loginMessage.style.display = 'block';
+
+    }
+    loginMessage.style.display = 'none';
     const details = {username,password};
-    if (username && password) {
       fetch(url, {
         method: 'POST',
         body: JSON.stringify(details),
@@ -317,8 +340,13 @@ const signIn = () => {
       .then((data) => {
         if(data.message === 'invalid username'){
           loginMessage.style.display = 'block';
-          loginMessage.style.color = 'red';
-          return loginMessage.textContent = 'invalid username'
+          loginMessage.style.backgroundColor = 'red';
+          return loginMessage.textContent = 'username entered is invalid'
+        }
+        if(data.message === 'invalid password'){
+          loginMessage.style.display = 'block';
+          loginMessage.style.backgroundColor = 'red';
+          return loginMessage.textContent = 'password entered is invalid'
         }
         if (data.status == 'Success') {
           localStorage.setItem('fastfoodUser',data.token);
@@ -329,6 +357,7 @@ const signIn = () => {
               loginMessage.style.display = 'block';
               return  window.location.href = 'admin.html';
             }else if (decoded.usertype === 'fastf00DuSER_$1'){
+              loginMessage.style.backgroundColor = 'green';
               loginMessage.textContent = 'login successful'
               loginMessage.style.display = 'block';
               return  window.location.href = 'user.html';
@@ -345,7 +374,7 @@ const signIn = () => {
         console.log('error: ',err);
       })
       
-    }
+    
   });
 
 };
