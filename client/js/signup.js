6692eab66ce45723da4c4be3171c10e0acdc9305
johnmadakin <edafeadjeke@gmail.com>
2018-10-04 23:47:16 +0100

    chore: fixed validation for both sign up and sign up - refactor codes for effective validation - validated users can seamlessly register or login
    [delivers #160992125]

--- client/js/signup.js
+++ client/js/signup.js
@@ -12,7 +12,17 @@ const modal = document.getElementById('modal-id');
 const loginUser = document.querySelector('.submit');
 const message = document.querySelector('.pop-up-message');
 const validEmail = document.querySelector('.email');
+const username = document.querySelector('.username');
+const firstname = document.querySelector('.firstname');
+const lastname = document.querySelector('.lastname');
+const phoneNo = document.querySelector('.phone-no');
+const deliveryAd = document.querySelector('.delivery-address');
+let imageLink = '';
+let isImageUpload = false
+
 const waiting = document.querySelector('.waiting');
+const loginMessage = document.querySelector('.login-message');
+let validated = false;
 
 /** https://code.lengstorf.com/get-form-values-as-json/
  * This function takes in an array of element and converts it to json
@@ -49,22 +59,40 @@ const checkPwdEquality = () => {
     }
     return pwdLabel.style.display = 'none';
   });
-}
+};
+
+const validateText = (text) => {
+  const validText = /^[0-9a-zA-Z\s #$&()%;,_@+|?!\.\-]+$/;
+  if (text.length >= 10 && text.length <= 150 && !!text.match(validText)) {
+    return true;
+  }
+  return false;
+};
 
 const checkValidKeys = key => {
   return key.name && key.value;
 };
 
-const validateForm = (formdata) => {
-  const formInputs = Object.keys(formdata)
-  formInputs.forEach(element => {
-    if(formdata[element] === '') {
-      popUp.style.display = 'block';
-      message.textContent = "field can't be empty";
-    }
-  })
+const validateUserText = (string) => {
+  if (typeof string !== 'string') return false;
+  if (string.length < 6 || string.length > 20 ) {
+    return false;
+  }
+  const validString = /^[0-9a-zA-Z_]+$/;
+  return string.trim().match(validString);
+};
 
-}
+ const validatePhoneNo = (phone) => {
+  const validPhoneChar = /^[0][7-9][0-9][0-9]\d{7}$/g;
+  return phone.trim().match(validPhoneChar);
+};
+
+ const validateString = (string) => {
+  if (typeof string !== 'string') return false;
+  if (string.length < 2 || string.length > 25) return false;
+  const validString = /^[a-zA-Z-]+$/;
+  return string.trim().match(validString);
+};
 
 const checkValidEmail = (email) => {
   const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -73,58 +101,153 @@ const checkValidEmail = (email) => {
   return true;
 };
 
-
-
-const postForm = (formData, uploadImage) => {
+const uploadImageToServer = () => {
   const url = 'https://api.cloudinary.com/v1_1/fast-food-fast/upload';
-  const url2 = `${baseUrl}/api/v1/auth/signup`;
-  popUp.style.display = 'block';
-  message.textContent = 'please wait... signing you up';
-  fetch(url, {
-    method: 'POST',
-    body: uploadImage,
-  })
-  .then(res => res.json())
-  .then((data)=>{
-    if(data.secure_url !== ''){
-      formData.imageUrl = data.secure_url;
-      return  fetch(url2, {
-        method: "POST",
-        body: JSON.stringify(formData),
-        headers: {
-          "Content-Type": "application/json",
+  const preset = 'usersprofileimages';
+  image.addEventListener('change', () => {
+    loginMessage.style.display = 'block';
+    loginMessage.style.backgroundColor = 'orange';
+    loginMessage.textContent = 'uploading image, please wait';
+    submit.disabled = true;
+    const imageUrl = image.files[0];
+    const uploadImage = new FormData();
+    uploadImage.append('file', imageUrl);
+    uploadImage.append("upload_preset", preset);
+    fetch(url, {
+      method: 'POST',
+      body: uploadImage,
+    })
+    .then(res => res.json())
+    .then((data)=>{
+      if(data.secure_url !== ''){
+        submit.disabled = false;
+        loginMessage.style.display = 'block';
+        loginMessage.style.backgroundColor = 'green';
+        loginMessage.textContent = 'image uploaded';
+        imageLink = data.secure_url;
+        isImageUpload = true;
+        return imageLink;
+      }
+      imageLink = '';
+    })
+    .catch((err)=>{
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = 'couldnt upload image';
+    })
+  });
+}
+
+const postForm = (formData) => {
+  const url = `${baseUrl}/api/v1/auth/signup`;
+  if(imageLink == ''){
+    isImageUpload = true;
+  }
+  if(isImageUpload === true ){
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => res.json())
+      .then((data) => {
+        if (data.status == 'Success') {
+          loginMessage.style.display = 'block';
+          loginMessage.style.backgroundColor = 'red';
+          loginMessage.textContent = 'Successfully Signed up';
+          localStorage.setItem('fastfoodUser', data.token);
+          return window.location.href = 'user.html';
+        } else if (data.status !== 'Success') {
+          loginMessage.style.display = 'block';
+          loginMessage.style.backgroundColor = 'red';
+          loginMessage.textContent = `${data.message}`;
+  
         }
       })
-    }
+    .catch(err => {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = 'Couldnt sign you up';
   })
-  .then((res)=> res.json())
-  .then((data) => {
-    if (data.status == 'Success') {
-      localStorage.setItem('fastfoodUser',data.token);
-      return  window.location.href = 'user.html';
-    } else if((data.status !== 'Success')){
-      return window.location.href = 'signup.html';
-    }
-  })
-  .catch(err => {
-    popUp.style.display = 'none';
-  })
-
+  }
+ 
 };
+
+const validateStringLength = (string) => {
+  if (string.length >= 6 && string.length <= 300) {
+    return true;
+  }
+  return false;
+};
+
+const validateImageUrl = (url) => {
+  const reg = /\.(jpe?g|png|)$/i;
+  if (reg.test(url)) return true;
+  return false;
+};
+
+function validateForm (formData) {
+  const formInputs = Object.keys(formData)
+  for(element of formInputs) {
+    if(element === 'email' && !checkValidEmail(formData[element])) {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `invalid email`;
+      return false;
+    }
+    if(element === 'password' && !validateStringLength(formData[element])) {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `invalid password length`;
+      return false;
+    }
+    if(element === 'username' && !validateUserText(formData[element])) {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `invalid username entered`;
+      return false;
+    }
+    if(element === 'firstname' && !validateString(formData[element])) {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `invalid firstname entered`;
+      return false;
+    }
+    if(element === 'lastname' && !validateString(formData[element])) {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `invalid lastname entered`;
+      return false;
+    }
+    if(element === 'phoneNo' && !validatePhoneNo(formData[element])) {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `invalid phone number entered`;
+      return false;
+    }
+    if(element === 'deliveryAddress' && !validateText(formData[element])) {
+      loginMessage.style.display = 'block';
+      loginMessage.style.backgroundColor = 'red';
+      loginMessage.textContent = `invalid address entered`;
+      return false;
+    }
+
+  }
+
+}
 
 const submitForm = (e) => {
   e.preventDefault();
-  const preset = 'usersprofileimages';
+  loginMessage.style.display = 'none';
   const formData = convertFormValues(formValues.elements);
-  validateForm(formData)
-  const imageUrl = image.files[0];
-  const uploadImage = new FormData();
-  uploadImage.append('file',imageUrl);
-  uploadImage.append("upload_preset", preset);
-  if(formData.confirmpassword === formData.password) {
-    delete formData.confirmpassword;
-    return postForm(formData, uploadImage);
+  delete formData.confirmpassword;
+  formData.imageUrl = imageLink;
+  if(validateForm(formData) === false){
+    return false;
   }
+  return postForm(formData);
 };
 
 const closePop = (e) =>{
@@ -134,14 +257,9 @@ const closePop = (e) =>{
 const startApp = ()  => {
   checkPasswordEquality();
   checkPwdEquality();
+  uploadImageToServer();
   closePopup.addEventListener('click', closePop);
-  submit.addEventListener('click', submitForm);
-  validEmail.addEventListener('blur', () => {
-    if(!checkValidEmail(validEmail.value)){
-      popUp.style.display = 'block';
-      message.textContent = 'invalid email format';
-    }
-  });
+  formValues.addEventListener('submit', submitForm);
   window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -149,6 +267,5 @@ const startApp = ()  => {
   }
 
 }
-
 
 startApp();
