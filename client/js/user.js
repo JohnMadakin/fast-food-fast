b0a35c7e72920e09888a5814bebea368fcb9538a
johnmadakin <edafeadjeke@gmail.com>
2018-10-10 17:00:18 +0100

    feature: user can view orders history [delivers #160977309]

--- client/js/user.js
+++ client/js/user.js
@@ -1,15 +1,19 @@
 const foodList = document.querySelectorAll('.food-item-list');
+const contentContainer = document.querySelectorAll('.admin-content-food');
 const adminNav = document.querySelectorAll('.admin-nav-orders');
+const adminContents = document.querySelectorAll('.admin-orders');
 const popUp = document.querySelector('.pop-up');
 const confirm = document.querySelector('.pop-up-open');
 const cancel = document.querySelector('.cancel');
 const editTitle = document.querySelector('.food-imgcontainer-title');
-const baseUrl = 'http://localhost:3002';
+const baseUrl =  'https://edafe-fast-food-fast.herokuapp.com';
 const adminContent = document.querySelector('.admin-content');
 const orderContainer = document.querySelector('.order-menu-group');
 const userProfileContainer = document.querySelector('.userProfileContainer');
 const logout = document.querySelector('.logout');
 const defaultImage = 'https://res.cloudinary.com/fast-food-fast/image/upload/v1538500928/usersprofileimages/img_avatar_m4xaqf.png';
+let userId;
+
 const logoutUsers = () => {
   logout.addEventListener('click',(e) => {
     localStorage.removeItem('fastfoodUser');
@@ -21,7 +25,7 @@ const verifyUsers = () => {
   const token = localStorage.getItem('fastfoodUser');
   try{
     const decoded = jwt_decode(token);
-    console.log(decoded)
+    userId = decoded.id;
     if(decoded) {
       const pageTitle = createNode('h1');
       let imgSrc;
@@ -30,14 +34,14 @@ const verifyUsers = () => {
       pageTitle.textContent = `Hello ${decoded.firstname}, Welcome to your dashboard`;
       const userImage = createNode('img');
       appendNode(userProfileContainer, userImage);
+
       userImage.className = 'userProfile';
       if(decoded.imageurl === ""){
         imgSrc = defaultImage;
         userImage.setAttribute('src',imgSrc);
         return;
       }
-      userImage.setAttribute('src', decoded.imageUrl);
-      // getUserOrders(decoded);
+      userImage.setAttribute('src', decoded.imageurl || decoded.imageUrl );
       return;
     }else {
       return window.location.href = 'signup.html';
@@ -48,13 +52,6 @@ const verifyUsers = () => {
   }
 }
 
-const getUserOrders = (user) => {
-  fetch(`${baseUrl}/api/v1/orders/${user.id}`)
-  .then((res) => res.json())
-  .then(data => {
-    // populateUserInfo(data, user);
-  })
-}
 
 const createNode = (element) => {
   return document.createElement(element);
@@ -104,6 +101,76 @@ function setFocus (e){
   e.target.className += " active";
 }
 
+const loadOrders = () => {
+  const token = localStorage.getItem('fastfoodUser');
+  const url = `${baseUrl}/api/v1/users/2/orders`;
+  fetch(url,  {
+    method: 'GET',
+    headers: {
+      "x-auth": token,
+    }
+  })
+  .then(res => res.json())
+  .then((data) => {
+    if(data.message === 'order not found'){
+      document.querySelector('.admin-title').textContent = `You have No pending Orders`;
+      document.querySelector('.order-status').textContent = `You have No confirmed Orders`;
+      adminContents.forEach((each)=>{
+        each.innerHTML = '';
+      });
+    }
+    if(data.status === 'Success'){
+      const userPending = document.querySelector('#user-pending');
+      const userConfirm = document.querySelector('#user-confirm');
+      data.ordersItem.forEach((order,i) => {
+        if(data.ordersInfo[i].orderstatus === 'pending'){
+          getOrders(userPending,data.ordersInfo[i],order,i);
+        }
+        if(data.ordersInfo[i].orderstatus === 'confirmed'){
+          getOrders(userConfirm,data.ordersInfo[i],order,i);
+        }
+
+      });
+    }
+  })
+  .catch((err)=> {
+    console.log(err)
+    // waiting.style.display = 'none';
+  });
+}
+
+const getOrders = (userPanel,ordersInfo,order,i) => {
+  const userOrder = document.createElement('div');
+  userOrder.innerHTML = `<div class="admin-order">
+  <h2 class="order-status">${ordersInfo.orderstatus}</h2>
+  
+  <div class="order-menu"><h3 class="order-menu-title">Order</h3>
+    <div class="admin-arrow"></div>
+  </div>
+  <div class="order-menu-group">
+      <p class="payment-status">Payment Status</p>
+      <span >${ordersInfo.paymentmethod}</span>
+      <p class="admin-food-list">Food List</p>
+      <div class="admin-food-items">
+        <ul class='user-food-list'>
+        </ul>
+      </div>
+      <h3 class="order-status confirmed">Home Address:${ordersInfo.deliveryaddress}</h3>  
+  </div>
+  
+</div>`;
+userPanel.appendChild(userOrder);
+const userFoodList = document.querySelectorAll('.user-food-list');
+order.forEach((item )=> {
+  const itemList = document.createElement('li');
+  itemList.className = 'admin-food-item';
+  itemList.innerHTML = `${item.title} <span class="admin-order-qty">${item.quantity}</span>`;
+  userFoodList[i].appendChild(itemList);
+});
+
+}
+
 verifyUsers();
 toggleprofileNav();
 logoutUsers();
+loadOrders();
